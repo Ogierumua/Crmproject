f6fa2c5ca73cd3fdc081e9d5685e1169ed42fc08
Ogierumua <ogierumuaanthony@gmail.com>
2023-12-21 16:08:53 -0600

    create a super user, run your server and test the super user, go to urls.py in your project directory and add include to the imported path module, add your application url under your admin url, in the urlpattern list data structure, go to your application directory and create a new file and call urls.py to hold your templates pages urls, imnport path and  and views,  create a list for urlpatterns to hold the template pages urls and their paths,  go to views.py file in your application directory and create a index function logic for index page, create a new directory in your project directory and call it templates and create htmls files for index and base, then register the template directory in settings.py, add html boiler plate to the base.html and add block content to the body of the base.html then extend the block content to your index.html file

--- crmapp/views.py
+++ crmapp/views.py
@@ -5,4 +5,8 @@ import mysql.connector
 con = mysql.connector.connect(host = 'localhost', user = 'root', password = 'Hailmary@12345.', database = 'crmbase')
 cursor = con.cursor()
 
+def index(request):
+    
+    return render(request, 'index.html', {})
+
 # Create your views here.
