3c77ad204ff04b088e44409dfd66da8586c14464
Ogierumua <ogierumuaanthony@gmail.com>
2024-01-26 14:40:24 -0600

    go to views.py in the app directory, import authenticate, login and logout from django.contrib.auth for authentication, then we want django to flash in and out messages when one signs in or out by importing messages from django.contrib, go to urls.py and create in your app directory and create a path for sigin, go to views.py and create a function logic for signin go to template directory and create a signin.html file to render the signin details

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signout', views.signout, name='signout'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
+    path('back', views.back, name='back')
 ]
--- crmapp/views.py
+++ crmapp/views.py
@@ -1,12 +1,82 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import mysql.connector
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 con = mysql.connector.connect(host = 'localhost', user = 'root', password = 'Hailmary@12345.', database = 'crmbase')
 cursor = con.cursor()
 
+
+
 def index(request):
     
-    return render(request, 'index.html', {})
+    return render(request, 'index.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been successfully logged in")
+            return redirect('signin')
+        else:
+            messages.warning(request, "you were not sucessfully logged in bad credentials")
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+def signup(request):
+    
+    return render(request, 'signup.html')
+
+def signout(request):
+    logout(request)
+    messages.success(request, "you have been Logged out successfully")
+    return redirect('index')
+    
+    
+
+def back(request):
+    
+    return render(request, 'index.html')
+    
+
+"""
+def index(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "you have successfully logged in")
+            return redirect('index')
+        else:
+            messages.warning(request, "You were not successfully looged in enter correct credentials")
+            return redirect('index')
+    else:
+        return render(request, 'index.html', {})
+    
+    
+    
+    
+def logout(request):
+    logout(request)
+    messages.success(request, "you have successfully logged out")
+    return redirect("index")
+
+#def index(request):
+    
+     #return render(request, 'index.html', {})
+    
+    
+    
+"""
 
 # Create your views here.
